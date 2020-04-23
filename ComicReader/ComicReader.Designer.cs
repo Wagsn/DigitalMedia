f42fedbe4e1b80148298a43246be89c3b7da788a
Wagsn <wagsn@foxmail.com>
2020-04-24 00:56:58 +0800

    漫画阅读器

---
+++ ComicReader/ComicReader.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ComicReader
+{
+    partial class ComicReader
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zoomBox = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开文件夹ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // zoomBox
+            // 
+            this.zoomBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.zoomBox.Location = new System.Drawing.Point(0, 28);
+            this.zoomBox.Name = "zoomBox";
+            this.zoomBox.Size = new System.Drawing.Size(733, 630);
+            this.zoomBox.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件FToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(733, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件FToolStripMenuItem
+            // 
+            this.文件FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开文件夹ToolStripMenuItem});
+            this.文件FToolStripMenuItem.Name = "文件FToolStripMenuItem";
+            this.文件FToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.文件FToolStripMenuItem.Text = "文件(&F)";
+            // 
+            // 打开文件夹ToolStripMenuItem
+            // 
+            this.打开文件夹ToolStripMenuItem.Name = "打开文件夹ToolStripMenuItem";
+            this.打开文件夹ToolStripMenuItem.Size = new System.Drawing.Size(167, 26);
+            this.打开文件夹ToolStripMenuItem.Text = "打开文件夹";
+            this.打开文件夹ToolStripMenuItem.Click += new System.EventHandler(this.打开文件夹ToolStripMenuItem_Click);
+            // 
+            // ComicReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 658);
+            this.Controls.Add(this.zoomBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "ComicReader";
+            this.Text = "ComicReader";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel zoomBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开文件夹ToolStripMenuItem;
+    }
+}
+
---
+++ ComicReader/ComicReader.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComicReader
+{
+    public partial class ComicReader : Form
+    {
+        public ComicReader()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFolder(string folder)
+        {
+            var files = Directory.GetFiles(folder);
+            int lastY = 0;
+            for (var i = 0; i < files.Length; i++)
+            {
+                var filename = files[i];
+                // load image
+                var image = Image.FromFile(filename);
+                var iw = image.Width;
+                var ih = image.Height;
+                var whRadio = iw / (double)ih;
+                var zw = zoomBox.Width;
+                var pw = zw;
+                var ph = pw / whRadio;
+
+                var pb = new PictureBox
+                {
+                    Location = new Point(0, lastY),
+                    Size = new Size(pw, (int)Math.Round(ph)),
+                    Image = image,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                };
+                zoomBox.Controls.Add(pb);
+                lastY += pb.Height;
+            }
+            _zoomW = zoomBox.Width;
+
+            this.zoomBox.Resize += ComicReader_Resize;
+            this.zoomBox.MouseWheel += ComicReader_MouseWheel;
+        }
+
+        private int _zoomW = 0;
+        // Y轴起始点，不可靠，建议改为起始Y轴比(_startYRadio)（Reseize不会修改其值，只会在Wheel中修改）
+        private int _startY = 0;
+        private double _startYRadio = 0.00;
+
+        /// <summary>
+        /// 鼠标滚动
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComicReader_MouseWheel(object sender, MouseEventArgs e)
+        {
+            int totalH = GetTotalHight();
+            var startY = (int)(totalH * _startYRadio);
+            if (e.Delta > 0)  // 向上滚
+            {
+                startY += 50;
+            }
+            else
+            {
+                startY -= 50;
+            }
+            _startYRadio = startY / (int)totalH;
+
+            ResizePictureBox();
+        }
+
+        private void ComicReader_Resize(object sender, EventArgs e)
+        {
+            var zoomW = zoomBox.Width;
+            _zoomW = zoomW;
+
+            ResizePictureBox();
+        }
+
+        private void ResizePictureBox()
+        {
+            int lastY = 0;
+            int totalH = GetTotalHight();
+            var startY = (int)(totalH * _startYRadio);
+            foreach (PictureBox pictureBox1 in zoomBox.Controls)
+            {
+                var image = pictureBox1.Image;
+                var iw = image.Width;
+                var ih = image.Height;
+                var whRadio = iw / (double)ih;
+                var zw = zoomBox.Width;
+                var pw = zw;
+                var ph = pw / whRadio;
+                pictureBox1.Location = new Point(0, lastY + startY);
+                pictureBox1.Size = new Size(pw, (int)ph);
+                lastY += pictureBox1.Height;
+            }
+        }
+
+        private int GetTotalHight()
+        {
+            int totalH = 0;
+            foreach (PictureBox pictureBox1 in zoomBox.Controls)
+            {
+                totalH += pictureBox1.Height;
+            }
+
+            return totalH;
+        }
+
+        private void 打开文件夹ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = Path.GetPathRoot(AppContext.BaseDirectory);
+            var dr = fbd.ShowDialog();
+            if(dr == DialogResult.OK)
+            {
+                var folder = fbd.SelectedPath;
+                OpenFolder(folder);
+            }
+        }
+    }
+}
